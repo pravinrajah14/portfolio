0ce8a62c167325201fc64c5a923b44aa9b9d82e9
Pravin Rajah <pravin.rajah@gmail.com>
2025-09-01 16:33:36 -0700

    Ignore linting issues

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Make a static site in ./out
   output: "export",
-  // If you ever use <Image>, this avoids server-side optimization
   images: { unoptimized: true },
-  // Optional: nicer URLs on static hosts
   trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
